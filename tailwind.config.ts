8779ade027e4b5dfa9ca802b69bda089fb289c0b
Hacker-Man07 <sayantan207@gmail.com>
2025-09-28 20:17:30 +0530

    Deployment Requirements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,10 @@ const config = {
       },
     },
     extend: {
-      
+      backgroundImage: {
+        'card-gradient-dark': 'linear-gradient(to top right, #232526, #434343)',
+        'card-gradient-light': 'linear-gradient(to top right, #ECE9E6, #FFFFFF)',
+      },
       fontFamily: {
         michroma: ['Michroma', 'sans-serif'],
       },
@@ -80,11 +83,23 @@ const config = {
             "background-position": "calc(100% + var(--shiny-width)) 0",
           },
         },
+        "glowing-border": {
+          "0%": {
+            "background-position": "0% 50%",
+          },
+          "50%": {
+            "background-position": "100% 50%",
+          },
+          "100%": {
+            "background-position": "0% 50%",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "shiny-text": "shiny-text 8s infinite",
+        "glowing-border": "glowing-border 6s linear infinite",
       },
     },
   },
